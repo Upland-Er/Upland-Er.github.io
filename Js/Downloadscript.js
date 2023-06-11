389f8ede8d64032205863c69cb5fa06ce36f2e38
Upland_er <NJOC2012@outlook.ie>
2023-06-11 15:41:47 +0100

    yes

--- Js/Downloadscript.js
+++
@@ -1,5 +0,0 @@
-function blinktext() {
-    var f = document.getElementById('Blinker');
-    setInterval(function() {
-      f.style.visibility = (f.style.visibility == 'hidden' ? '' : 'hidden');
-      }, 100);}
