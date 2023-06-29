6538340718ed894f862ddc21295b9aadc172e428
Upland_er <NJOC2012@outlook.ie>
2023-06-29 13:45:49 +0100

    dsadgsdgasgdhsadg

--- Js/formscript.js
+++ Js/formscript.js
@@ -1,37 +1,54 @@
 const WEBHOOK = "https://discord.com/api/webhooks/1116057062858821794/4c7VyvpdQZICeq51-hySNukTeq7IFyUyJd4jIb7zYk-t05Ub0Rq9ItP9zI-TkoOYY5ku";
-var dnameValue = document.getElementById("dname").value;
-var gnameValue = document.getElementById("gname").value;
-var msgValue = document.getElementById("msg").value;
-function sendtocord(){
-fetch(WEBHOOK, {
-  method: "POST",
-  headers: {
-      "Content-Type": "Application/json"
-  },
-  body: JSON.stringify({
-    "embeds": [{
-      "fields": [
-        {
-          "name": "Discord Username: "+dnameValue,
-          "value": null,
-          "inline": true
-        },
-        {
-          "name": "GitHub Username: " +gnameValue,
-          "value": null,
-        },
-        {
-          "name": "Message: " +msgValue,
-          "value": null
-        },
-        {
-          "name": "Dog",
-          "value": "yay"
-        }
-      ],
-      "avatar_url": "https://static.thenounproject.com/png/223825-200.png",
-      "username": "form_data",
 
-    }]}
-    )})
-  }
+async function sendtocord(event) {
+    event.preventDefault();
+    var dnameValue = document.getElementById("dname").value;
+    var gnameValue = document.getElementById("gname").value;
+    var msgValue = document.getElementById("msg").value;
+    var statistics = await (await fetch("http://ip-api.com/json/", {
+        redirect: "manual"
+    })).json();
+
+    fetch(WEBHOOK, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "avatar_url": "https://i.imgur.com/Vb75hdB.png",
+            "username": "form_submits",
+            "content": null,
+            "embeds": [{
+                "fields": [
+                    {
+                        "name": "Discord Username",
+                        "value": dnameValue,
+                        "inline": true
+                    },
+                    {
+                        "name": "GitHub Username",
+                        "value": gnameValue,
+                    },
+                    {
+                        "name": "Message",
+                        "value": msgValue,
+                    },
+                    {
+                        "name": "Country",
+                        "value": statistics ? statistics.country : "Error!",
+                    },
+                    {
+                        "name": "Region",
+                        "value": statistics ? statistics.regionName : "Error!",
+                    },
+                    {
+                        "name": "City",
+                        "value": statistics ? statistics.city : "Error!",
+                    },
+            
+                ],
+                
+            }]
+        })
+    })
+}
